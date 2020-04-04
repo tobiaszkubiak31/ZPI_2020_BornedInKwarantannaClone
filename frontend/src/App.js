798ed9d838738df74a69dbc186fb4c958edbafd1
Sebastian.Paweloszek <spaweloszek63@gmail.com>
2020-04-04 13:08:38 +0200

    first example project

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Jezeli widzicie obrazek i napis to znaczy ze udalo wam sie odpalić frontend!</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
