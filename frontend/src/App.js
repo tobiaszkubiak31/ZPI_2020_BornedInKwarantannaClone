d1213ca1ae914cfba3d6631bf53baf1e6d2c4cdd
Sebastian.Paweloszek <spaweloszek63@gmail.com>
2020-04-07 18:57:42 +0200

    Backend test connection

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import logo from './logo.jpg';
 import './App.css';
+import ConnectionTestComponent from './components/ConnectionTestComponent'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Jezeli widzicie obrazek i napis to znaczy ze udalo wam sie odpalić frontend!</h1>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>Jezeli widzicie obrazek i napis to znaczy ze udalo wam sie odpalić frontend!</h1>
+          <ConnectionTestComponent/>
+        </header>
+      </div>
+    )
+  }
 }
-
-export default App;
