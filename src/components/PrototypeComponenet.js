3b332ac762a7afb0f25474c7e7337238f14253af
Sebastian.Paweloszek <spaweloszek63@gmail.com>
2020-04-08 13:51:14 +0200

    Created empty class file

---
+++ src/components/PrototypeComponenet.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default class PrototypeComponent extends React.Component {
+
+    render() {
+        return (
+            <div>ELO</div>
+        )
+    }
+
+}
