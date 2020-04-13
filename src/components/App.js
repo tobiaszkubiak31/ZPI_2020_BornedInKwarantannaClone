a28c361b2ba032c130a07cf2be2c53d34c8263d8
Mmati1996 <57298066+Mmati1996@users.noreply.github.com>
2020-04-13 16:33:40 +0200

    Add files via upload
    usuniecie obrazka

--- src/components/App.js
+++ src/components/App.js
@@ -9,7 +9,6 @@ export default class App extends React.Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1>Jezeli widzicie obrazek i napis to znaczy ze udalo wam sie odpalić frontend!</h1>
           <ConnectionTestComponent/>
         </header>
