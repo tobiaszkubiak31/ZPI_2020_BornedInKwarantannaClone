446944ef33c9dd1550523df4131a2678925cfff9
Sebastian.Paweloszek <spaweloszek63@gmail.com>
2020-04-20 19:41:06 +0200

    initialize empty test file

---
+++ src/__tests__/PrototypeComponent.test.js
@@ -0,0 +1,4 @@
+import React from 'react'
+import PrototypeComponent from '../components/PrototypeComponenet'
+import { render } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
