96bc6efe2da7cce7801bf0551136b554e920b46b
Sebastian.Paweloszek <spaweloszek63@gmail.com>
2020-04-20 20:53:12 +0200

    validator test

--- src/__tests__/PrototypeComponent.test.js
+++ src/__tests__/PrototypeComponent.test.js
@@ -1,4 +1,20 @@
 import React from 'react'
 import PrototypeComponent from '../components/PrototypeComponenet'
-import { render } from '@testing-library/react'
+import { render, getByLabelText } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
+import {validateNumber} from '../utils/ValidatingFunctions'
+
+test('validateNumber function should return false with string', async = () => {
+    const toTest = "abcdefg"
+    expect(validateNumber(toTest)).toBe(false)
+})
+
+test('validateNumber function should return false with numbers and letters', async = () => {
+    const toTest = "123asd"
+    expect(validateNumber(toTest)).toBe(false)
+})
+
+test('validateNumber function should return true', async = () => {
+    const toTest = "100"
+    expect(validateNumber(toTest)).toBe(true)
+})
