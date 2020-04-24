1b713970518d2afb544114981e3266b6af779f99
Sebastian.Paweloszek <spaweloszek63@gmail.com>
2020-04-24 15:43:02 +0200

    remove unnecessary elements

--- src/components/App.js
+++ src/components/App.js
@@ -8,8 +8,6 @@ export default class App extends React.Component {
     return (
       <div className="App">
         <header className="App-header">
-          <h1>Jezeli widzicie i napis to znaczy ze udalo wam sie odpalić frontend!</h1>
-          <ConnectionTestComponent/>
         </header>
         {/*Tutaj jest nasz nowy element*/}
         <PrototypeComponent/>
