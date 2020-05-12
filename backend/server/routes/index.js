9bd210578ae52be1c52f5835b0ef8bfaf7735cac
Patryk Stępień <patryk.stepien@automotivedev.pl>
2020-05-12 16:25:16 +0200

    Use router in server.js file and update gitignore

--- backend/server/routes/index.js
+++ backend/server/routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/', (res, req, next) => {
+router.get('/', (req, res, next) => {
 
     res.json({test: 'test'});
 
--- backend/server/server.js
+++ backend/server/server.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const apiRouter = require('./routes');
 
 const app = express();
 
 app.use(express.json());
+
+app.use('/json', apiRouter);
 
 app.listen(process.env.PORT || '3000', () => {
 
